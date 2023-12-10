7a1692220ea1b5578e9ad5d52b4e92f71454e5c1
santn <kevinsan@edu.aau.at>
2023-12-10 14:47:50 +0100

    added fxml files and controllers

---
+++ src/main/java/com/library/library_system/LibraryLoginController.java
@@ -0,0 +1,4 @@
+package com.library.library_system;
+
+public class LibraryLoginController {
+}
--- src/main/java/com/library/library_system/LibraryStart.java
+++ src/main/java/com/library/library_system/LibraryStart.java
@@ -11,8 +11,8 @@ public class LibraryStart extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(LibraryStart.class.getResource("library-start-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        stage.setTitle("Library System");
         stage.setScene(scene);
         stage.show();
     }
