67bc1a79cfc6b18f3ab56b7815df1fa1adc248ee
Max Krassnitzer <m1krassnit@edu.aau.at>
2024-01-04 19:51:22 +0100

    added book propertys

--- src/main/java/bll/Book.java
+++ src/main/java/bll/Book.java
@@ -7,12 +7,18 @@ import java.util.List;
 
 public class Book {
     private String title;
+    private String author;
     private String isbn;
+    private int pages;
+    private Date publicationDate;
     private Status status;
 
-    public Book(String title, String isbn, Status status) {
+    public Book(String title, String author, String isbn, int pages, Date publicationDate, Status status) {
         this.title = title;
+        this.author = author;
         this.isbn = isbn;
+        this.pages = pages;
+        this.publicationDate = publicationDate;
         this.status = status;
     }
 
@@ -26,6 +32,42 @@ public class Book {
 
     public Status getStatus() {
         return status;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public Date getPublicationDate() {
+        return publicationDate;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public void setPublicationDate(Date publicationDate) {
+        this.publicationDate = publicationDate;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
     }
 
     public static void writeData(Book book) {
